010c5cb853783c1f20706819068b7fed7172f467
Damian Michałowski <damianmichalowskidm@gmail.com>
2025-06-02 22:14:24 +0200

    home layout

---
+++ src/app/(home)/about/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Aboutp age</div>
+}
+
+export default Page
---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return (
+    <footer className="flex border-t justify-between font-medium p-6">
+      <div className="flex items-center gap-2">
+        <p>funroad, Inc.</p>
+        <p>All rights reserved.</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <p>Privacy Policy</p>
+        <p>Terms of Service</p>
+      </div>
+    </footer>
+  )
+}
